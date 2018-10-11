b12114394e7b083e0f181626c28bcd2a52a8e075
Darko <darkopranjic.4@gmail.com>
2018-10-11 14:04:58 +0200

    Added nested descritions and reusability for test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -25,4 +25,46 @@ describe("WHEN LOGGED IN", async () => {
     const label = await page.getElement("form label");
     expect(label).toEqual("Blog Title");
   });
+
+  describe("And using valid inputs", async () => {
+    const titleText = "My test title";
+    const contentText = "My test content";
+
+    beforeEach(async () => {
+      await page.type(".title input", titleText);
+      await page.type(".content input", contentText);
+      await page.click("form button");
+    });
+
+    test("Submitting takes user to review screen", async () => {
+      const text = await page.getElement("form h5");
+      expect(text).toEqual("Please confirm your entries");
+    });
+
+    test("Submitting then saving adds blog to index page", async () => {
+      await page.click("button.green");
+
+      await page.waitFor(".card");
+
+      const title = await page.getElement(".card-title");
+      const content = await page.getElement("p");
+
+      expect(title).toEqual(titleText);
+      expect(content).toEqual(contentText);
+    });
+  });
+
+  describe("And using invalid inputs", async () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+
+    test("The form shows invalid inputs", async () => {
+      const titleErr = await page.getElement(".title .red-text");
+      const contentErr = await page.getElement(".content .red-text");
+
+      expect(titleErr).toEqual("You must provide a value");
+      expect(contentErr).toEqual("You must provide a value");
+    });
+  });
 });
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -8,9 +8,7 @@ class CustomPage {
   }
 
   static async build() {
-    const browser = await puppeteer.launch({
-      headless: false
-    });
+    const browser = await puppeteer.launch({});
 
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
