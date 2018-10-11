229189c2996a709e871b3e353762b870c24dc17a
Darko <darkopranjic.4@gmail.com>
2018-10-11 20:52:18 +0200

    EDIT (Wrong env set in travis env, Adding in two records causes two separate builds to be created, where one build has the NODE_ENV set, and the other has PORT set.  ) Added enviroment keys for CI. Set serving of React app from server side in CI virtual machine

--- services/cache.js
+++ services/cache.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
-const redisUrl = "redis://127.0.0.1:6379";
-const client = redis.createClient(redisUrl);
+const keys = require("../config/keys");
+
+const client = redis.createClient(keys.redisUrl);
 const util = require("util");
 
 client.hget = util.promisify(client.hget);
