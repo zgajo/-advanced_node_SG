6fc982f85f3a98c3732c911061401f23d71638a4
Darko <darkopranjic.4@gmail.com>
2018-10-12 11:08:06 +0200

    Adding image upload

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -1,12 +1,16 @@
 // BlogFormReview shows users their form inputs for review
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
 class BlogFormReview extends Component {
+  state = {
+    file: null
+  };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -39,12 +43,18 @@ class BlogFormReview extends Component {
     );
   }
 
+  onFileChange(event) {
+    this.setState({
+      file: event.target.files
+    });
+  }
+
   onSubmit(event) {
     event.preventDefault();
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
   }
 
   render() {
@@ -52,6 +62,13 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an image</h5>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={this.onFileChange.bind(this)}
+        />
 
         {this.renderButtons()}
       </form>
@@ -63,4 +80,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.blogForm.values };
 }
 
-export default connect(mapStateToProps, actions)(withRouter(BlogFormReview));
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(BlogFormReview));
